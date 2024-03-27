ef0379bb118a0136a4be65f96e00552ccd98b7aa
VibhinnS <vibhinnsinghal25@gmail.com>
2024-03-27 17:23:28 +0530

    Improved UI/UX of admin panel

---
+++ nexspace/admin.py
@@ -0,0 +1,15 @@
+# admin.py
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from unfold.admin import ModelAdmin
+
+
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    pass
