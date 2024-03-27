55f19a4961c7e7f365eedbfa029e573e1ac07de5
VibhinnS <vibhinnsinghal25@gmail.com>
2024-03-27 15:34:35 +0530

    Added gitignore and comments

--- nexspace/views.py
+++ nexspace/views.py
@@ -12,6 +12,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 
+
+#OAuth Registration
 oauth = OAuth()
 
 oauth.register(
